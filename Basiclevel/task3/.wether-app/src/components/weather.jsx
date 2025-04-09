c4984fb7c496b0fc3e52f46f1a000341e0421ad7
safan-1234 <safwanbegar786@gmail.com>
2025-04-09 18:01:51 +0530

    Initial commit for Basic Level project

---
+++ Basiclevel/task3/.wether-app/src/components/weather.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import "./weather.css";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import search_icon from "../assets/search.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+
+const Weather = () => {
+  const search=async(city)=>{
+    try{
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_APIKEY}`;
+
+      const response=await fetch(url);
+      const data=await response.json();
+      console.log(data);
+    }catch (error)
+    {
+
+    }
+  }
+  useEffect(()=>{
+    search("London")
+  })
+  return (
+    <div className="weather">
+      <div className="Search-bar">
+        <input type="text" placeholder="Search..." />
+        <img src={search_icon} alt="search icon" />
+      </div>
+      <img src={clear_icon} alt="" className='weather-icon'/>
+      <p className="temperature">16°</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+<img src={humidity_icon} alt="" />
+<div>
+  <p>91 %</p>
+  <span>Humidity</span>
+</div>
+        </div>
+        <div className="col"></div>
+        <img src={wind_icon} alt="" />
+        <div>
+          <p>3.6km/h</p>
+          <span>Wind Speed</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
